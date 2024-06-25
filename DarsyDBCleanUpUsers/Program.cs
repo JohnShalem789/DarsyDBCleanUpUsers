23772c5c5c6e69183c77edb4be52bc5f5da21129
Tottaramudi Shalem <v-tshalem@microsoft.com>
2024-06-25 16:40:23 +0530

    Added Changes related to Reading from DB and Checking InActiveUsers

--- DarsyDBCleanUpUsers/Program.cs
+++ DarsyDBCleanUpUsers/Program.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using System.Configuration;
+using System.Data.SqlClient;
+using System.Globalization;
 using System.Net.Http.Headers;
 using System.Reflection.Metadata;
 using System.Text.Encodings.Web;
 using System.Text.Json;
+using static DarsyDBCleanUpUsers.ViewModels;
 
 
 namespace DarsyDBCleanUpUsers
@@ -12,43 +16,33 @@ namespace DarsyDBCleanUpUsers
     {
         public static void Main(string[] args)
         {
-            GraphApiService graphApiService = new GraphApiService();
-            graphApiService.CallGraphAPI();
+            try
+            {
+                DarsyUpdateInactiveUsers.RetriveAndProcessUsers();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred in the main process: {ex.Message}");
+            }
+
         }
     }
 }
 
-public class GraphApiService
+public class DarsyUpdateInactiveUsers
 {   
-    public void CallGraphAPI()
+    public static string CallGraphAPI(RequestViewModel requestViewModel)
     {
+        string responseContent = String.Empty;
+
         try
         {
-            string token = ConfigurationManager.AppSettings["RequestUrl"];
-            string requesturl = ConfigurationManager.AppSettings["BearerToken"];
-
-
-            
-            
-
-            var searchRequestByEmail = new
-            {
-                requests = new[]
-                {
-                    new
-                    {
-                        entityTypes = new[]{"person"},
-                        query = new
-                        {
-                            queryString = "v-tshalem"
-                        }
-                    }
-                }
-            };
-
+            string requesturl = ConfigurationManager.AppSettings["RequestUrl"];
+            string token = ConfigurationManager.AppSettings["BearerToken"];
+           
             HttpClient httpClient = new HttpClient();
 
-            string jsonRequest = Newtonsoft.Json.JsonConvert.SerializeObject(searchRequestByEmail);
+            string jsonRequest = Newtonsoft.Json.JsonConvert.SerializeObject(requestViewModel);
 
             HttpContent content = new StringContent(jsonRequest, System.Text.Encoding.UTF8, "application/json");
 
@@ -56,13 +50,116 @@ public class GraphApiService
 
             HttpResponseMessage response = httpClient.PostAsync(requesturl, content).Result;
 
-            string responseContent = response.Content.ReadAsStringAsync().Result;
+            responseContent = response.Content.ReadAsStringAsync().Result;            
 
-            Console.WriteLine(responseContent);
+            if (response.IsSuccessStatusCode)
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine("API call successful. Response:");
+            }
+            else
+            {
+                Console.WriteLine($"Error calling API. Status code: {response.StatusCode}");
+            }         
+        }
+        catch (HttpRequestException httpEx)
+        {
+            Console.WriteLine($"HTTP Request Error: {httpEx.Message}");
         }
         catch (Exception ex)
         {
-            Console.WriteLine(ex.Message.ToString());
+            Console.WriteLine($"General Error: {ex.Message}");
+        }
+
+        return responseContent;
+    }
+
+    public static void RetriveAndProcessUsers()
+    {
+        string connectionString = ConfigurationManager.AppSettings["DbConnection"];
+        
+        try
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                Console.WriteLine("Connection Successful");
+                string query = "SELECT UserEmail, UserAlias, UserDomain FROM Users";
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string userAlias = reader["UserAlias"].ToString();
+                    string userDomain = reader["UserDomain"].ToString();
+                    string userEmail = reader["UserEmail"].ToString();
+
+                    var searchRequest = new RequestViewModel
+                    {
+                        requests = new UserRequestViewModel[] {
+                            new UserRequestViewModel()
+                            {
+                                entityTypes = new[] { "person" },
+                                query = new QueryViewModel
+                                {
+                                    queryString = $"{userEmail}"
+                                }
+                            }
+                        }
+                    };
+
+                    Console.WriteLine($"Started Checking User by Graph API: {userEmail}");
+                    string responseContent = CallGraphAPI(searchRequest);
+                    Console.WriteLine($"Response from Graph API: {responseContent}");
+
+                    // Check if the user exists based on the API response
+                    bool userExists = CheckIfUserExists(responseContent);
+
+                    if (userExists)
+                    {
+                        Console.WriteLine($"User '{userEmail}' exists in the Graph API response.");
+                        // Perform further actions if the user exists
+                    }
+                    else
+                    {
+                        Console.WriteLine($"User '{userEmail}' does not exist in the Graph API response.");
+                        // Perform actions for non-existent users
+                    }
+
+                    Console.WriteLine($"Completed Checking User by Graph API: {userEmail}");
+                    Console.WriteLine(); 
+
+                }              
+                reader.Close();
+            }
+        }
+        catch (SqlException sqlEx)
+        {
+            Console.WriteLine($"SQL Error: {sqlEx.Message}");
+            // Optionally log the exception to a file or other logging mechanism
+            throw new Exception("Error retrieving data from database.", sqlEx);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error constructing request: {ex.Message}");
+            // Optionally log the exception to a file or other logging mechanism
+            throw; // Rethrow the exception to be caught in the Main method
+        }
+    }
+
+    public static bool CheckIfUserExists(string responseContent)
+    {
+        try
+        {
+            dynamic responseObject = JsonConvert.DeserializeObject(responseContent);
+            int totalHits = responseObject.value[0].hitsContainers[0].total;
+
+            return totalHits > 0;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error parsing Graph API response: {ex.Message}");
+            return false; // Return false in case of any error or unexpected response structure
         }
     }
 }
---
+++ DarsyDBCleanUpUsers/ViewModels.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarsyDBCleanUpUsers
+{
+    public class ViewModels
+    {
+        public class UserRequestViewModel
+        {
+            public string[] entityTypes { get; set; }
+            public QueryViewModel query { get; set; }
+        }
+
+        public class QueryViewModel
+        {
+            public string queryString { get; set; }
+        }
+
+        public class RequestViewModel
+        {
+            public UserRequestViewModel[] requests { get; set; }
+        }
+
+    }
+}
