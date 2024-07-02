8673e2273ab5f79538a210a941a270d722b40ab3
Tottaramudi Shalem <v-tshalem@microsoft.com>
2024-07-02 21:10:26 +0530

    Adding Changes For Null Columns

---
+++ DarsyDBCleanUpUsers/CheckingForInActiveUsers.cs
@@ -0,0 +1,221 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using static DarsyDBCleanUpUsers.ViewModels;
+
+namespace DarsyDBCleanUpUsers
+{
+    public static class DarsyUpdateInactiveUsers
+    {
+        public static string connectionString = ConfigurationManager.AppSettings["DbConnection"];
+        public static HttpResponseMessage CallGraphAPI(RequestViewModel requestViewModel)
+        {
+            string responseContent = String.Empty;
+            HttpResponseMessage httpResponseMessage = new HttpResponseMessage();
+            try
+            {
+                string requesturl = ConfigurationManager.AppSettings["RequestUrl"];
+                string token = ConfigurationManager.AppSettings["BearerToken"];
+
+                HttpClient httpClient = new HttpClient();
+
+                string jsonRequest = Newtonsoft.Json.JsonConvert.SerializeObject(requestViewModel);
+
+                HttpContent content = new StringContent(jsonRequest, System.Text.Encoding.UTF8, "application/json");
+
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                httpResponseMessage = httpClient.PostAsync(requesturl, content).Result;
+
+                if (httpResponseMessage.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("API call successful. Response:");
+                }
+                else
+                {
+                    Console.WriteLine($"Error calling API. Status code: {httpResponseMessage.StatusCode}");
+                    throw new Exception("Error Calling the API, Please check the Bearer Token");
+                }
+            }
+            catch (HttpRequestException httpEx)
+            {
+                Console.WriteLine($"HTTP Request Error: {httpEx.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"General Error: {ex.Message}");
+            }
+
+            return httpResponseMessage;
+        }
+
+        public static void RetriveAndProcessUsers()
+        {
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Connection Successful");
+                    string query = "SELECT UserEmail, UserAlias, UserDomain,IsActive FROM Users";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        string userAlias = reader["UserAlias"].ToString();
+                        string userDomain = reader["UserDomain"].ToString();
+                        string userEmail = reader["UserEmail"].ToString();
+                        bool isActive = (bool)reader["IsActive"];
+
+                        var searchRequest = new RequestViewModel
+                        {
+                            requests = new UserRequestViewModel[] {
+                            new UserRequestViewModel()
+                            {
+                                entityTypes = new[] { "person" },
+                                query = new QueryViewModel
+                                {
+                                    queryString = $"{userEmail}"
+                                }
+                            }
+                        }
+                        };
+
+                        Console.WriteLine($"Started Checking User by Graph API: {userEmail}");
+                        var httpResponseMessage = CallGraphAPI(searchRequest);
+                        string responseContent = httpResponseMessage.Content.ReadAsStringAsync().Result;
+                        Console.WriteLine($"Response from Graph API: {responseContent}");
+
+                        // Check if the user exists based on the API response,Not enters if StatusCode is Not 200;  
+                        if (httpResponseMessage.IsSuccessStatusCode)
+                        {
+                            bool userExists = CheckIfUserExistsV1(responseContent);
+
+                            if (userExists)
+                            {
+                                Console.WriteLine($"User '{userEmail}' exists in the Graph API response.");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"User '{userEmail}' does not exist in the Graph API response.");
+                                UpdateUserIsActiveStatus(userEmail);
+                            }
+
+                            Console.WriteLine($"Completed Checking User by Graph API: {userEmail}");
+                            Console.WriteLine();
+                        }
+                    }
+                    reader.Close();
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                Console.WriteLine($"SQL Error: {sqlEx.Message}");
+                // Optionally log the exception to a file or other logging mechanism
+                throw new Exception("Error retrieving data from database.", sqlEx);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error constructing request: {ex.Message}");
+                // Optionally log the exception to a file or other logging mechanism
+                throw; // Rethrow the exception to be caught in the Main method
+            }
+        }
+
+        public static bool CheckIfUserExists(string responseContent)
+        {
+            try
+            {
+                dynamic responseObject = JsonConvert.DeserializeObject(responseContent);
+                int totalHits = responseObject.value[0].hitsContainers[0].total;
+
+                return totalHits > 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error parsing Graph API response: {ex.Message}");
+                return false; // Return false in case of any error or unexpected response structure
+            }
+        }
+
+        public static bool CheckIfUserExistsV1(string responseContent)
+        {
+            try
+            {
+                dynamic responseObject = JsonConvert.DeserializeObject(responseContent);
+                // Check if there are any hits
+                if (responseObject?.value.Count > 0 && responseObject?.value[0].hitsContainers.Count > 0)
+                {
+                    var hitsContainer = responseObject?.value[0].hitsContainers[0];
+                    if (hitsContainer?.total > 0)
+                    {
+                        // Check if the user is an OrganizationUser
+                        var resource = hitsContainer.hits[0].resource;
+                        if (resource != null && resource?.companyName == "Microsoft" &&
+                            resource?.personType != null && resource?.personType.@class == "Person" &&
+                            resource?.personType.subclass == "OrganizationUser")
+                        {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error parsing Graph API response: {ex.Message}");
+                return false; // Return false in case of any error or unexpected response structure
+            }
+        }
+
+
+
+        public static void UpdateUserIsActiveStatus(string userEmail)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (SqlTransaction transaction = connection.BeginTransaction())
+                    {
+                        string updateQuery = "UPDATE Users SET IsActive = @IsActive WHERE UserEmail = @UserEmail";
+                        SqlCommand updateCommand = new SqlCommand(updateQuery, connection, transaction);
+                        updateCommand.Parameters.AddWithValue("@IsActive", 0);
+                        updateCommand.Parameters.AddWithValue("@UserEmail", userEmail);
+
+                        int rowsAffected = updateCommand.ExecuteNonQuery();
+                        transaction.Commit();
+                        if (rowsAffected > 0)
+                        {
+                            Console.WriteLine($"Successfully updated IsActive status for user '{userEmail}' to 0.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"No rows were updated for user '{userEmail}'.");
+                        }
+                    }
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                Console.WriteLine($"SQL Error while updating IsActive status: {sqlEx.Message}");
+                // Optionally log the exception to a file or other logging mechanism
+                throw new Exception("Error updating IsActive status in database.", sqlEx);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"General Error while updating IsActive status: {ex.Message}");
+                // Optionally log the exception to a file or other logging mechanism            
+            }
+        }
+    }
+}
---
+++ DarsyDBCleanUpUsers/CheckingForNullsColumns.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarsyDBCleanUpUsers
+{
+    public static class CheckingForNullsColumns
+    {
+        public static void CheckingForNullColumns()
+        {
+            string connectionString = "Server=(LocalDb)\\MSSQLLocalDB;Database=DarsyTestDB;Trusted_Connection=True;";
+            //string connectionString = "data source=tcp:darsydevserver.database.windows.net,1433;initial catalog=DevToolsDB;persist security info=True;User ID=DevDBUser;Password=D@rsyT00ls20@!;MultipleActiveResultSets=True;Connection Timeout=250;App=EntityFramework";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("Connection successful!");
+                    DataTable tablesSchema = connection.GetSchema("Tables");
+
+                    foreach (DataRow table in tablesSchema.Rows)
+                    {
+                        string tableName = table["TABLE_NAME"].ToString();
+                        Console.WriteLine($"Checking table: {tableName}");
+
+                        CheckColumnsForNulls(connection, tableName);
+                        Console.WriteLine(new string('-', 50));
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine($"SQL exception: {ex.Message}");
+                }
+                catch (InvalidOperationException ex)
+                {
+                    Console.WriteLine($"Invalid operation: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+                }
+                finally
+                {
+                    if (connection.State == ConnectionState.Open)
+                    {
+                        connection.Close();
+                        Console.WriteLine("Connection closed.");
+                    }
+                }
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static void CheckColumnsForNulls(SqlConnection connection, string tableName)
+        {
+            try
+            {
+                DataTable columnsSchema = connection.GetSchema("Columns", new[] { null, null, tableName });
+
+                foreach (DataRow column in columnsSchema.Rows)
+                {
+                    string columnName = column["COLUMN_NAME"].ToString();
+                    CheckColumnForNulls(connection, tableName, columnName);
+                    CheckForLastUpdateColumn(connection, tableName, columnName);
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine($"SQL exception while processing table {tableName}: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while processing table {tableName}: {ex.Message}");
+            }
+        }
+
+        private static void CheckColumnForNulls(SqlConnection connection, string tableName, string columnName)
+        {
+            try
+            {
+                string query1 = $"SELECT COUNT(*) FROM [{tableName}] WHERE [{columnName}] IS NULL";
+                string query2 = $"SELECT COUNT(*) FROM [{tableName}] WHERE [{columnName}] IS NOT NULL";
+                using (SqlCommand command1 = new SqlCommand(query1, connection))
+                using (SqlCommand command2 = new SqlCommand(query2, connection))
+                {
+                    int countNulls = Convert.ToInt32(command1.ExecuteScalar());
+                    int countNotNulls = Convert.ToInt32(command2.ExecuteScalar());
+                    int Totalrows = countNulls + countNotNulls;
+                    if (countNulls > 0)
+                    {
+                        Console.WriteLine($"Table :'{tableName}', Column :'{columnName}', Total Column:'{countNulls + countNotNulls}',  NULL value(s): '{countNulls}'  NOT NULL value(s):'{countNotNulls}' .");
+                    }
+
+                    else if (countNulls == Totalrows)
+                    {
+
+                        Console.WriteLine($"Table '{tableName}', '{columnName}' entire column NULL");
+                    }
+
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine($"SQL exception while checking nulls in {tableName}.{columnName}: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while checking nulls in {tableName}.{columnName}: {ex.Message}");
+            }
+        }
+
+        private static void CheckForLastUpdateColumn(SqlConnection connection, string tableName, string columnName)
+        {
+            if (columnName.ToLower().Contains("last") && columnName.ToLower().Contains("update") && !columnName.ToLower().Contains("by"))
+            {
+                try
+                {
+                    string lastUpdateQuery = $"SELECT MAX([{columnName}]) FROM [{tableName}]";
+                    string firstUpdateQuery = $"SELECT MIN([{columnName}]) FROM [{tableName}]";
+                    using (SqlCommand lastUpdateCommand = new SqlCommand(lastUpdateQuery, connection))
+                    using (SqlCommand firstUpdateCommand = new SqlCommand(firstUpdateQuery, connection))
+                    {
+                        object lastUpdateDate = lastUpdateCommand.ExecuteScalar();
+                        object firstUpdateDate = firstUpdateCommand.ExecuteScalar();
+                        if (lastUpdateDate != DBNull.Value)
+                        {
+                            Console.WriteLine($"Table '{tableName}' was first updated on: {firstUpdateDate}");
+                            Console.WriteLine($"Table '{tableName}' was last updated on: {lastUpdateDate}");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Table '{tableName}' has no updates recorded in column '{columnName}'.");
+                        }
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine($"SQL exception while checking last update in {tableName}.{columnName}: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred while checking last update in {tableName}.{columnName}: {ex.Message}");
+                }
+            }
+        }
+
+    }
+}
